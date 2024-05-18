3e81a14a745fc8535b85101c8562b4ba5912accd
unknown <atomsiwawut@gmail.com>
2024-05-18 13:52:18 +0700

    pleno2

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,35 +1,33 @@
-import React from 'react'
-import '@/assets/styles/globals.css'
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
-import AuthProvider from '@/components/AuthProvider';
-
-import { ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import "@/assets/styles/globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import AuthProvider from "@/components/AuthProvider";
+import { GlobalProvider } from "@/context/GlobalContext";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const metadata = {
-  title: 'PropertyPulse | Find The Perfect Place',
-  description: ' Find The Best Place',
-  keywords: 'Asset Manangement'
+  title: "PropertyPulse | Find The Perfect Place",
+  description: " Find The Best Place",
+  keywords: "Asset Manangement",
 };
 
 const Mainlayout = ({ children }) => {
   return (
-    <AuthProvider>
-      <html lang='en'>
-        <body>
-          <Navbar />
-          <main>{children}</main>
-          <Footer />
-          <ToastContainer/>
-        </body>
-      </html>
-
-
-    </AuthProvider>
-
-
+    <GlobalProvider>
+      <AuthProvider>
+        <html lang="en">
+          <body>
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+            <ToastContainer />
+          </body>
+        </html>
+      </AuthProvider>
+    </GlobalProvider>
   );
 };
 
-export default Mainlayout
+export default Mainlayout;
