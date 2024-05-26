5d5cf17b1c485d7c32379510288abb460bcf672d
unknown <atomsiwawut@gmail.com>
2024-05-26 11:31:50 +0700

    “Update“

--- app/api/properties/[id]/route.js
+++ app/api/properties/[id]/route.js
@@ -20,43 +20,58 @@ export const GET = async (request, { params }) => {
   }
 };
 
-// Delete /api/properties/id
-// DELETE /api/properties/id
+// DELETE /api/properties/:id
+// NOTE: Here we also need to delete images from Cloudinary
+ 
 export const DELETE = async (request, { params }) => {
   try {
     const propertyId = params.id;
-
-    // Initialize database connection
-    await connectDB();
-
-    // Check for session and get session user
+ 
     const sessionUser = await getSessionUser();
+ 
+    // Check for session
     if (!sessionUser || !sessionUser.userId) {
-      return new Response("User ID is Required", { status: 401 });
+      return new Response('User ID is required', { status: 401 });
     }
-
-    // Find the property by ID
+ 
+    const { userId } = sessionUser;
+ 
+    await connectDB();
+ 
     const property = await Property.findById(propertyId);
-    if (!property) {
-      return new Response("Property Not Found", { status: 404 });
+ 
+    if (!property) return new Response('Property Not Found', { status: 404 });
+ 
+    // Verify ownership
+    if (property.owner.toString() !== userId) {
+      return new Response('Unauthorized', { status: 401 });
     }
-
-    // Verify property ownership
-    if (property.owner.toString() !== sessionUser.userId) {
-      return new Response("Unauthorized", { status: 401 });
+ 
+    // extract public id's from image url in DB
+    const publicIds = property.images.map((imageUrl) => {
+      const parts = imageUrl.split('/');
+      return parts.at(-1).split('.').at(0);
+    });
+ 
+    // Delete images from Cloudinary
+    if (publicIds.length > 0) {
+      for (let publicId of publicIds) {
+        await cloudinary.uploader.destroy('propertypulse/' + publicId);
+      }
     }
-
-    // Delete the property
+ 
+    // Proceed with property deletion
     await property.deleteOne();
-
-    return new Response("Property Deleted", {
+ 
+    return new Response('Property Deleted', {
       status: 200,
     });
   } catch (error) {
     console.log(error);
-    return new Response("Something Went Wrong", { status: 500 });
+    return new Response('Something Went Wrong', { status: 500 });
   }
 };
+
 
 // PUT /api/properties/:id
 export const PUT = async (request, { params }) => {
